6d41c6bd8cc8e666d72c59a082f266fdda2eaf27
rahul06030 <rahul06030@gmail.com>
2021-06-25 10:53:17 +0530

    settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-c1cg^-j%2l-e_-(0+ey030&yvz@^k$x@%w0pwgm#of%)qrqe+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS =["127.0.0.1", "*.heroku.com"]
+ALLOWED_HOSTS =["127.0.0.1", "rahul06030.herokuapp.com"]
 
 
 # Application definition

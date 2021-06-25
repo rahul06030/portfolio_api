35c57dc07b4de1695ac4f2d044da7470b376f82e
rahul06030 <rahul06030@gmail.com>
2021-06-25 10:06:20 +0530

    settings.py]

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -45,11 +45,11 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
 'django.middleware.security.SecurityMiddleware',
-          'corsheaders.middleware.CorsMiddleware',
-      'whitenoise.middleware.WhiteNoiseMiddleware',
+        'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+           'corsheaders.middleware.CorsMiddleware',
+ 'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -139,7 +139,8 @@ STATIC_URL = '/static/'
 
 # django_heroku.settings(locals())
 
-# CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
+ROOT_UTLCONF ='portfolio.urls'
 # CORS_ORIGIN_WHITELIST = (
 #   'http://localhost:8000',
 # )
